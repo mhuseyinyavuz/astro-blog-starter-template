1c6524113f3e98d560823953cb4f6f4e80ca0d41
mhuseyinyavuz <eeehuseyinyavuz@gmail.com>
2025-09-26 11:03:50 +0300

    Astro sayfa güncellendi: QR kod, matrix efekt, dark/light toggle eklendi.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,19 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react"; // <-- küçük harf
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
-
+  integrations: [mdx(), sitemap(), react()], // <-- küçük harf
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
   }),
-
   vite: {
     plugins: [tailwindcss()],
   },
